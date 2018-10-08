081028201587659cb35bf6a12880af87ca671780
guzzi <guzzijones12@gmail.com>
2018-10-08 09:10:26 -0400

    add readme information and github info to package

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
-
+with open("README.md","r") as fh:
+    long_description = fh.read()
 setup(
     name='demistoapi',
-    version='0.3',
+    version='0.4',
     packages=[
         "demistoapi"
     ],
     license='MIT',
-    long_description="Demisto API SDK",
+    long_description=long_description,
     install_requires=[
        "requests",
         "urllib3"
